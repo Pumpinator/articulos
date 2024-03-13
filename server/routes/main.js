5860d6d14ef738a49e83b33b0e990aa617e08eb8
ElGeka13 <geka0479@gmail.com>
2024-03-12 23:10:02 -0600

    Login and Signup forms

--- server/routes/main.js
+++ server/routes/main.js
@@ -6,7 +6,19 @@ router.get('/', (req, resp) => {
         title: "Inicio | Articulos",
         active: true
     }
-    resp.render("index", {locals});
+    resp.render("index", { locals });
+})
+
+router.get('/registro', (req, resp) => {
+    resp.render("signup");
+})
+
+router.get('/ingreso', (req, resp) => {
+    resp.render("login");
+})
+
+router.get('/articulo/registro', (req, resp) => {
+    resp.render("articleForm");
 })
 
 module.exports = router;
