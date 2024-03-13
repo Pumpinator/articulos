e418e369980bfdf71c3113335fd965ae41a7c640
Pumpinator <alejandrodcardona5@gmail.com>
2024-03-12 20:04:02 -0600

    Project skeleton with home page

--- server/routes/main.js
+++ server/routes/main.js
@@ -2,7 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 router.get('/', (req, resp) => {
-    resp.render("index");
+    const locals = {
+        title: "Inicio | Articulos",
+        active: true
+    }
+    resp.render("index", {locals});
 })
 
 module.exports = router;
